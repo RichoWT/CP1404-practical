b7353a9bad99fc4e676dfa1f4f1d62c73783195e
RichoWT <itsalmightyricho@gmail.com>
2024-09-23 21:06:15 +0800

    changed the code to functions

---
+++ prac_02/temperatures.py
@@ -0,0 +1,37 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+
+def main():
+    MENU = "C - Convert Celsius to Fahrenheit\nF - Convert Fahrenheit to Celsius\nQ - Quit"
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            fahrenheit = convert_celcius_to_farenheit()
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            celsius = convert_farenheit_to_celcius()
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_farenheit_to_celcius():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def convert_celcius_to_farenheit():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+main()
