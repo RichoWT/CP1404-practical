289e76664149287d93f2f171004e63f01ad325d8
RichoWT <itsalmightyricho@gmail.com>
2024-09-24 09:15:53 +0800

    finished score_py

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,11 +1,13 @@
 """
-CP1404/CP5632 - Practical NOT DONE YET
+CP1404/CP5632 - Practical 2
 """
+from random import randint
 
 
 def main():
     result = get_valid_score("Enter score: ", 0, 100)
     print(determine_grade(result))
+    print(f"The random grade is: {random_grade()}")
 
 
 def get_valid_score(prompt, low, high):
@@ -23,6 +25,12 @@ def determine_grade(score):
         return "pass"
     else:
         return "bad"
+
+
+def random_grade():
+    random_grade = randint(0, 100)
+    determined_grade = determine_grade(random_grade)
+    return determined_grade
 
 
 main()
