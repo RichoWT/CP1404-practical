344947029ee598590606c31fb68a64ddc6f48380
RichoWT <itsalmightyricho@gmail.com>
2024-10-11 20:48:03 +0800

    Finished the first part of list_exercises.py

---
+++ prac_04/list_exercise.py
@@ -0,0 +1,12 @@
+numbers = []
+for i in range(1, 6):
+    number = int(input("Number: "))
+    numbers.append(number)
+print(numbers)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+average = sum(numbers) / len(numbers)
+print(f"The average of the numbers {average}")
+
