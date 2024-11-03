08e06f92fe0c65a36373a7394fd0537f17e8d6e3
RichoWT <itsalmightyricho@gmail.com>
2024-11-03 15:38:50 +0800

    Create empty list and get input from user

---
+++ prac_06/guitars.py
@@ -0,0 +1,14 @@
+"""
+CP1404
+Time estimated: 30 minutes ( 3:07pm )
+Time finished:
+"""
+
+from prac_06.guitar import Guitar
+
+guitars = []
+
+print("My guitars!")
+name = input("Name: ")
+year = int(input("Year: "))
+cost = float(input("Cost: "))
