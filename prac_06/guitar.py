055423ae61d675af7f09773b545a675fac599d14
RichoWT <itsalmightyricho@gmail.com>
2024-11-03 15:12:51 +0800

    add class for guitar with init and str

---
+++ prac_06/guitar.py
@@ -0,0 +1,18 @@
+"""
+CP1404
+Time estimated: 30 minutes ( 3:07pm )
+Time finished:
+"""
+
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self):
