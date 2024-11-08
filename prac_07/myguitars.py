2f3c549ba0fef32ab01769a7e0213b758f5fb3c3
RichoWT <itsalmightyricho@gmail.com>
2024-11-09 00:44:49 +0800

    Add while loop for input and add a way to write input to csv file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -7,6 +7,8 @@ def main():
     guitars = load_guitars_from_csv(filename)
     guitars.sort()
     show_guitars(guitars)
+    get_new_guitar(guitars)
+    save_guitars(filename, guitars)
 
 
 def load_guitars_from_csv(filename):
@@ -23,6 +25,24 @@ def load_guitars_from_csv(filename):
 def show_guitars(guitars):
     for guitar in guitars:
         print(guitar)
+
+
+def get_new_guitar(guitars):
+    name = input("Name: ").strip()
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(f"{new_guitar} added.")
+        name = input("Name: ").strip()
+
+
+def save_guitars(filename, guitars):
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for guitar in guitars:
+            writer.writerow([guitar.name, guitar.year, guitar.cost])
 
 
 main()
