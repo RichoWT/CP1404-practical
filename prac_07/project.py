7ff9962b0d3e37d2cd8badc74ced4c6c95821b05
RichoWT <itsalmightyricho@gmail.com>
2024-11-10 15:56:16 +0800

    Add error checking for choice and percentage

--- prac_07/project.py
+++ prac_07/project.py
@@ -15,7 +15,7 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
-        return f"({self.name}, {self.start_date}, {self.priority}, {self.cost_estimate}, {self.completion_percentage})"
+        return f"{self.name}, {self.start_date}, {self.priority}, {self.cost_estimate}, {self.completion_percentage}"
 
     def is_complete(self):
         return self.completion_percentage == 100
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,9 +4,9 @@ time started: 5:10pm (estimate - 1 day)
 
 """
 
-
 from prac_07.project import Project
 import datetime
+
 FILENAME = "projects.txt"
 
 
@@ -14,11 +14,14 @@ def main():
     print("Welcome to Pythonic Project Management")
     projects = get_data(FILENAME)
     print(f"5 places loaded from {FILENAME}")
-    print("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n- (U)pdate project\n- (Q)uit ")
+    print(
+        "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n- (U)pdate project\n- (Q)uit ")
     option = input(">>> ").upper()
     while option != "Q":
+
         if option == "L":
             print("Projects loaded.")
+
         elif option == "S":
             print("test2")
 
@@ -39,14 +42,25 @@ def main():
 
         elif option == "F":
             print("test4")
+
         elif option == "A":
             print("test5")
+
         elif option == "U":
-            print("test6")
+            i = 1
+            for project in projects:
+                print(f"{i}. {project}")
+                i += 1
+            choice = get_valid_choice(projects)
+            print(projects[choice - 1])
+
         else:
             print("invalid option")
+        print(
+            "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n- (U)pdate project\n- (Q)uit ")
         option = input(">>> ").upper()
     print("Thank you!")
+
 
 def get_data(filename):
     projects = []
@@ -59,6 +73,30 @@ def get_data(filename):
         return projects
 
 
+def get_valid_choice(projects):
+    try:
+        choice = int(input("Project choice: "))
+        if choice < 0 or choice > len(projects):
+            print(f"Project choice must be between 1 and {len(projects)}")
+            return get_valid_choice(projects)
+        else:
+            return choice
+    except ValueError:
+        print("Invalid input")
+        return get_valid_choice(projects)
+
+
+def get_valid_percentage():
+    try:
+        percentage = int(input("Enter completion percentage (0-100): "))
+        if percentage < 0 or percentage > 100:
+            print("Completion percentage must be between 0 and 100.")
+            return get_valid_percentage()
+        else:
+            return percentage
+    except ValueError:
+        print("Invalid input")
+        return get_valid_percentage()
 
 
 main()
