1dd5871ede9e480e26d54bec29c954655162121c
RichoWT <itsalmightyricho@gmail.com>
2024-11-13 23:22:40 +0800

    Add function to convert input to float to multiply it with constant and convert it back to a string when the button is pressed

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,12 +1,34 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+
+FACTOR_MILES_TO_KM = 1.60934
 
 
 class ConvertMiles(App):
+
+    output_km = StringProperty()
+
     def build(self):
         self.title = 'Convert Miles to Kilometers'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_calculation(self, text):
+        print("handle calculation")
+        miles = self.convert_to_number(text)
+        self.update_result(miles)
+
+    def update_result(self, miles):
+        print("update")
+        self.output_km = str(miles * FACTOR_MILES_TO_KM)
+
+    def convert_to_number(self, text):
+        try:
+            return float(text)
+        except ValueError:
+            return 0.0
 
 
 ConvertMiles().run()
