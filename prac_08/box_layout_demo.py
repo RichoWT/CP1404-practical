e2058ef6c0aa53f81ccec2f17a65bce1f357d2e7
RichoWT <itsalmightyricho@gmail.com>
2024-11-14 00:47:34 +0800

    Add comment for box_layout_demo.py

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,16 +4,19 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Name title and load kv file """
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Print hello message"""
         print("test")
         self.root.ids.output_label.text = "Hello "
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Clear everything when button is pressed"""
         self.root.ids.input_name.text = ''
         self.root.ids.output_label.text = ''
 
