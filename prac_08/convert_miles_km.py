e96acf730dc6de0f4c1caafdff70364cd946d2b5
RichoWT <itsalmightyricho@gmail.com>
2024-11-14 00:49:46 +0800

    Add comment for convert_miles_km.py

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -11,25 +11,30 @@ class ConvertMiles(App):
     output_km = StringProperty()
 
     def build(self):
+        """title and load kv file"""
         self.title = 'Convert Miles to Kilometers'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_calculation(self, text):
+        """Act as the main"""
         print("handle calculation")
         miles = self.convert_to_number(text)
         self.update_result(miles)
 
     def handle_increment(self, text, change):
+        """Allow for incrementing numbers"""
         print("handle increment")
         miles = self.convert_to_number(text) + change
         self.root.ids.input_miles.text = str(miles)
 
     def update_result(self, miles):
+        """Calculate miles to km"""
         print("update")
         self.output_km = str(miles * FACTOR_MILES_TO_KM)
 
     def convert_to_number(self, text):
+        """convert string to float"""
         try:
             return float(text)
         except ValueError:
