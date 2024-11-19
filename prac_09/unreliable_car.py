52f0b3b499f890b95b7131c8998ef8b5b4b57cb2
RichoWT <itsalmightyricho@gmail.com>
2024-11-20 05:46:08 +0800

    Add test function to check for mistake, correct super() to call from parent class

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -5,10 +5,8 @@ from prac_09.car import Car
 class UnreliableCar(Car):
 
     def __init__(self, name, fuel, reliability):
-        self.name = name
-        self.fuel = fuel
+        super().__init__(name, fuel)
         self.reliability = reliability
-        super().__init__(name)
 
     def drive(self, distance):
         random_number = randint(1, 100)
--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -3,3 +3,11 @@ from prac_09.unreliable_car import UnreliableCar
 
 good_car = UnreliableCar("Good", 100,95)
 bad_car = UnreliableCar("Bad", 100,10)
+
+for i in range(12):
+    print(f"Test drive {i}km:")
+    print(f"{good_car.name} drove {good_car.drive(i)}km")
+    print(f"{bad_car.name} drove {bad_car.drive(i)}km")
+
+print(good_car)
+print(bad_car)
