5120038e2dc40cc7b9c85cb37a8795de9ee61115
RichoWT <itsalmightyricho@gmail.com>
2024-11-20 06:05:00 +0800

    Add str method and test to see if the class works or not

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,15 +5,16 @@ class SilverServiceTaxi(Taxi):
 
     flagfall = 4.5
 
-    def __init__(self, name, fuel, fanciness, price_per_km):
+    def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = fanciness
-        self.price_per_km = price_per_km
+        self.price_per_km *= fanciness
 
     def get_fare(self):
         return self.flagfall + super().get_fare()
 
-
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall}"
 
 
 
--- prac_09/silver_service_test_taxi.py
+++ prac_09/silver_service_test_taxi.py
@@ -1,1 +1,7 @@
 
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+taxi = SilverServiceTaxi("Silver Taxi", 100, 2)
+taxi.drive(18)
+print(taxi)
+print(f"${taxi.get_fare()}")
