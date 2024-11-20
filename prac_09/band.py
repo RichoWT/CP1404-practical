c5594de640dfb9a327adb939a2bd3845c746641b
RichoWT <itsalmightyricho@gmail.com>
2024-11-20 14:10:20 +0800

    Add a add method and play method to combine to results

--- prac_09/band.py
+++ prac_09/band.py
@@ -7,3 +7,11 @@ class Band:
     def __str__(self):
         return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
 
+    def add(self, musician):
+        self.musicians.append(musician)
+
+    def play(self):
+        results = []
+        for musician in self.musicians:
+            results.append(musician.play())
+        return "\n".join(results)
