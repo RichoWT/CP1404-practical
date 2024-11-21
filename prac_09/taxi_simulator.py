e22b1ab26fa78c55f8e91e02056d34b2ccc496c7
RichoWT <itsalmightyricho@gmail.com>
2024-11-21 17:37:34 +0800

    Add display to call methods from Taxi class and SilverServiceTaxi and function to add total cost together

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -6,9 +6,10 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
-    total_bill = 0
+    total_cost = 0
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
     print("Let's Drive!")
     print(MENU)
     option = input(">>> ").lower()
@@ -16,18 +17,36 @@ def main():
         if option == "c":
             print("Taxis available: ")
             display_taxis(taxis)
-            taxi_choice = int(input("Choose taxi: "))
-            print(taxi_choice)
+            try:
+                taxi_choice = int(input("Choose taxi: "))
+                if 0 <= taxi_choice < len(taxis):
+                    current_taxi = taxis[taxi_choice]
+                else:
+                    print("Invalid taxi choice")
+            except ValueError:
+                print("Invalid input; please enter a number.")
         elif option == "d":
-            print("2")
+            if current_taxi:
+                current_taxi.start_fare()
+                distance_to_drive = float(input("Drive how far? "))
+                current_taxi.drive(distance_to_drive)
+                travel_cost = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${travel_cost:.2f}")
+                total_cost += travel_cost
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
-            print("invalid option")
+            print("Invalid option")
+        print(f"Bill to date: ${total_cost:.2f}")
         print(MENU)
         option = input(">>> ").lower()
-    print("Thank you!")
+
+    print(f"Total trip cost: ${total_cost:.2f}")
+    print("Taxis are now:")
+    display_taxis(taxis)
 
 def display_taxis(taxis):
     for i, taxi in enumerate(taxis):
-        print(f"{i+1} - {taxi}")
+        print(f"{i} - {taxi}")
 
 main()
