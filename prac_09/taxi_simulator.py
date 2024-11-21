1aabf3e6d3493ff8b525b97bc52d500bd794a0f7
RichoWT <itsalmightyricho@gmail.com>
2024-11-21 14:24:22 +0800

    Add display function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -6,19 +6,28 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
     print("Let's Drive!")
     print(MENU)
-    option = input(">>> ")
+    option = input(">>> ").lower()
     while option != "q":
         if option == "c":
-            print("1")
+            print("Taxis available: ")
+            display_taxis(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            print(taxi_choice)
         elif option == "d":
             print("2")
         else:
             print("invalid option")
         print(MENU)
-        option = input(">>> ")
+        option = input(">>> ").lower()
     print("Thank you!")
 
+def display_taxis(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i+1} - {taxi}")
 
 main()
